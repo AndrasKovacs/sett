b4685bc44e4d43a2f6ff21c86f9b5719dfb5bbe7
Rafael Bocquet <bocquet@inf.elte.hu>
2022-11-11 11:17:53 +0100

    Add green/red colors for PASS/FAIL

--- test/Test.hs
+++ test/Test.hs
@@ -6,12 +6,15 @@ import Control.Monad
 import Data.List
 import System.Directory.Recursive
 import System.FilePath
+import System.IO
 import System.Exit
+import qualified System.Console.ANSI.Codes as ANSI
 
 import Paths_sett
 
 import Common
 import MainInteraction
+import System.Console.ANSI (setSGRCode)
 
 dropPrefix :: Eq a => [a] -> [a] -> [a]
 dropPrefix (x:xs) (y:ys) | x == y = dropPrefix xs ys
@@ -19,6 +22,14 @@ dropPrefix _      ys              = ys
 
 main :: IO ()
 main = do
+  inTerminal <- hIsTerminalDevice stdout
+  let withColor color str
+        = if inTerminal
+          then ANSI.setSGRCode [ANSI.SetColor ANSI.Foreground ANSI.Dull color] ++ str ++ ANSI.setSGRCode [ANSI.Reset]
+          else str
+  let passStr = withColor ANSI.Green "PASS"
+  let failStr = withColor ANSI.Red "FAIL"
+
   dir <- getDataDir
   let dropDir = dropPrefix dir
   succeedFiles <- getFilesRecursive (dir </> "succeed")
@@ -28,13 +39,13 @@ main = do
 
   succeedFails <- flip filterM succeedFiles \path -> do
     catch
-      (do {testElab path; putStrLn (dropDir path ++ " PASS"); pure False})
-      (\(e :: SomeException) -> do {putStrLn (dropDir path ++ " FAIL"); pure True})
+      (do {testElab path; putStrLn (dropDir path ++ " " ++ passStr); pure False})
+      (\(e :: SomeException) -> do {putStrLn (dropDir path ++ " " ++ failStr); pure True})
 
   failFails <- flip filterM failFiles \path -> do
     catch
-      (do {testElab path; putStrLn (dropDir path ++ " FAIL"); pure True})
-      (\(e :: SomeException) -> do {putStrLn (dropDir path ++" PASS"); pure False})
+      (do {testElab path; putStrLn (dropDir path ++ " " ++ failStr); pure True})
+      (\(e :: SomeException) -> do {putStrLn (dropDir path ++ " " ++ passStr); pure False})
 
   case (succeedFails, failFails) of
     ([], []) -> pure ()
